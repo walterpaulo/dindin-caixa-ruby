8faec2364dc0dc8ac7e002d8ef5aa3ca96fc6f5f
walterpaulo <walter0paulo@hotmail.com>
2021-12-25 20:36:17 -0300

    produto

---
+++ app/models/produto.rb
@@ -0,0 +1,4 @@
+class Produto < ApplicationRecord
+    validates :nome, presence: true
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211225184314) do
+ActiveRecord::Schema.define(version: 20211225231714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 20211225184314) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "produtos", force: :cascade do |t|
+    t.string   "nome"
+    t.text     "descricao"
+    t.integer  "quantidade"
+    t.datetime "vencimento"
+    t.float    "preco_compra"
+    t.float    "preco_venda"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
